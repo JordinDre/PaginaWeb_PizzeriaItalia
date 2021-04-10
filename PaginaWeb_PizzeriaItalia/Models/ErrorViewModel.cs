2e8cf0a051030fab2e681f8f0e210bc6e7c5ef4a
Jordin de León <jordindre77@outlook.com>
2021-04-09 22:37:55 -0600

    Agregar archivos de proyecto.

---
+++ PaginaWeb_PizzeriaItalia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PaginaWeb_PizzeriaItalia.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
